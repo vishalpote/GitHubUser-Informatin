8290b96e70a51c3942ef2fe3693946e3ff1fa338
vishal <vishalpote9090@gmail.com>
2023-09-29 13:46:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import UseEffectAPI from './GitHub/UseEffectApi'
+function App() {
+  
+
+  return (
+    <>
+      <h1>GitHub User Information</h1>
+      <UseEffectAPI></UseEffectAPI>
+    </>
+  )
+}
+
+export default App
